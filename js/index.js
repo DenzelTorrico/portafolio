58b0b45db0b78087505a3d3b5caec691f1c4f6fd
DenzelTorrico <denzel_silva@hotmail.com>
2024-02-22 16:59:46 -0500

    Java y Laravel Proyejcts

--- js/index.js
+++ js/index.js
@@ -3,23 +3,34 @@ let mercado = document.getElementById("Mercadoz")
 let spring = document.getElementById("proyectospring")
 let apiclima = document.getElementById("apiclima")
 let proyectmovies = document.getElementById("proyectmovies")
+let notificaciones = document.getElementById("sistemanotificaciones")
+let javainventario = document.getElementById("javainventario")
 
 let linkssistema = ["Cliente.png","egresos.png","ingreso.png"]
 let linksmercado = ["mercadoz2.png","mercadoz3.png","mercadoz4.png","mercadoz.png"]
 let linksspring = ["a.png","b.png","c.png","d.png"]
 let linksapiclima = ["a.png","b.png","c.png"]
 let linksproyectmovies = ["a.png","b.png","c.png","d.png"]
+let linksnotificaciones = ["a.png","b.png","c.png"]
+let linksjavainventario = ["a.png","b.png","c.png","d.png","e.png"]
 
-let contadorsistema = 0
-let contadormercado = 0
-let contadorspring = 0
-let contadorapiclima = 0
-let contadorproyectmovies = 0
+let [
+    contadorsistema,
+    contadormercado,
+    contadorspring,
+    contadorapiclima,
+    contadorproyectmovies,
+    CounterNotifications,
+    CounterJavaInventory,
+  ] = Array(7).fill(0);
+  
 let url = "./img/sistemacontable/"
 let urlm = "./img/proyectoventa/"
 let urls = "./img/proyectospring/"
 let urlapi = "./img/apiclima/"
 let urlmovie = "./img/proyectmovies/"
+let urlNoti = "./img/sistemanotififaciones/"
+let UrlInventory = "./img/javainventario/"
 
 let random  = setInterval(function() {
     sistema.src = url+linkssistema[contadorsistema]
@@ -27,11 +38,16 @@ let random  = setInterval(function() {
     spring.src = urls+linksspring[contadorspring]
     apiclima.src = urlapi+linksapiclima[contadorapiclima]
     proyectmovies.src = urlmovie+linksproyectmovies[contadorproyectmovies]
+    notificaciones.src = urlNoti+linksnotificaciones[CounterNotifications]
+    javainventario.src = UrlInventory+linksjavainventario[CounterJavaInventory]
+
     contadormercado++
     contadorsistema++
     contadorspring++
     contadorapiclima++
     contadorproyectmovies++
+    CounterNotifications++
+    CounterJavaInventory++
     if(linkssistema.length==contadorsistema){
         contadorsistema = 0
     }
@@ -44,8 +60,14 @@ let random  = setInterval(function() {
     if(linksapiclima.length == contadorapiclima){
         contadorapiclima = 0
     }
-    if(linksproyectmovies.length = contadorproyectmovies){
+    if(linksproyectmovies.length == contadorproyectmovies){
         contadorproyectmovies = 0
+    }
+    if(linksnotificaciones.length == CounterNotifications){
+        CounterNotifications = 0
+    }
+    if(linksjavainventario.length == CounterJavaInventory){
+        CounterJavaInventory = 0
     }
     
 },2000)
